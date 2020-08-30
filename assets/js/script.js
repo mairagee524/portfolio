4ace59ce11966517e1606f4683c34637cf8d2999
mairagee524 <garciamaira524@yahoo.com>
2020-08-29 20:22:42 -0700

    added code to restyle toolbox section

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,16 +41,16 @@ sr.reveal('.home__img',{delay: 400});
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
-sr.reveal('.section-title',{}); 
-sr.reveal('.about__img',{}); 
+sr.reveal('.section-title',{delay: 400}); 
+sr.reveal('.about__img',{delay: 400}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.image-group',{interval: 200}); 
+sr.reveal('.list-of-skills-no-pic',{delay: 600});
 
 /*SCROLL WORK*/
 // sr.reveal('.work__img',{interval: 200}); 
